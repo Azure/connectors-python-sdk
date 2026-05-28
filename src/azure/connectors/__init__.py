9585e4d7524bbfa5b6fba133cb4eadecd2794ca3
AzureConnectors <azurelogicapps@microsoft.com>
2026-05-28 19:04:59 +0000

    build: update Python Connectors SDK Version to 0.2.0b2

--- src/azure/connectors/__init__.py
+++ src/azure/connectors/__init__.py
@@ -60,7 +60,7 @@ try:
 except (ImportError, NameError):
     OnedriveClient = None  # type: ignore[assignment,misc]
 
-__version__ = '0.2.0b1'
+__version__ = '0.2.0b2'
 
 __all__ = [
     'ConnectorClientBase',
